5206ca203610c37e1a49a0439969446aa2ae2af8
SHAJAN29 <m.j.sandy2916@gmail.com>
2023-01-23 14:08:57 +0530

    Button Removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import React from 'react'
 import Button from '@mui/material/Button';
 import './App.css'
 import CheckIcon from '@mui/icons-material/Check';
@@ -6,14 +7,11 @@ import CloseIcon from '@mui/icons-material/Close';
 import Stack from '@mui/material/Stack';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <div className="App">
      
-     <Button variant="contained"> <CheckIcon/>Contained</Button>
-
-
 <Card price="0" plan="free" user="Single User" storage="5GB Storage" project="Unlimited Public Projects" access="Community Access" privateProjects="Unlimited Private Projects" support="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Reports"/>
 <Card price="9" plan="plus" user="5 Users" storage="50GB Storage" project="Unlimited Public Projects" access="Community Access" privateProjects="Unlimited Private Projects" support="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Reports"/>
 <Card price="49" plan="pro" user="Unlimited Users" storage="150GB Storage" project="Unlimited Public Projects" access="Community Access" privateProjects="Unlimited Private Projects" support="Dedicated Phone Support" subdomain="Free Subdomain" reports="Monthly Status Reports"/>
@@ -43,13 +41,13 @@ return(
               <li><span><CheckIcon/></span>{storage}</li>
               <li><span><CheckIcon/></span>{project}</li>
               <li><span><CheckIcon/></span>{access}</li>
-              <li class={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{privateProjects}
+              <li className={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{privateProjects}
                 </li>
-              <li class={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{support}
+              <li className={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{support}
                 </li>
-              <li class={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{subdomain}
+              <li className={plan=="plus" ?"text-display":plan=="pro" ?"text-display":"text-muted"}><span>{plan=="plus" ?<CheckIcon/>:plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{subdomain}
               </li>
-              <li class={plan=="pro" ?"text-display":"text-muted"}><span>{plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{reports}
+              <li className={plan=="pro" ?"text-display":"text-muted"}><span>{plan=="pro" ?<CheckIcon/>:<CloseIcon/>}</span>{reports}
                 </li>
             </ul>
             
